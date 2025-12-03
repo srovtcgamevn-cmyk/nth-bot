82968d647c45b0f71eb79e0466051dccc5dec524
srovtcgamevn-cmyk <sro.vtcgame.vn@gmail.com>
2025-12-04 00:38:20 +0700

    Update NTH5.3.py

--- NTH5.3.py
+++ NTH5.3.py
@@ -4562,10 +4562,10 @@ async def cmd_boctham(ctx: commands.Context):
 
     # ===== EMBED KẾT QUẢ =====
     if session_type == "event":
-        title = "🎲 BỐC THĂM MAY MẮN SỰ KIỆN"
+        title = "🎄 BỐC THĂM MAY MẮN SỰ KIỆN"
         color = 0xFFD700
     else:
-        title = "🎲 BỐC THĂM MAY MẮN"
+        title = "🎲 BỐC THĂM MAY MẮN THƯỜNG"
         color = 0x4CC9F0
 
     embed = discord.Embed(title=title, color=color)
